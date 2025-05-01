137180a1a875755bb201182b24b33b645af0d49a
Kvn3Toj <kpp.coomunity@gmail.com>
2025-04-30 19:23:24 -0500

    chore: configure for static hosting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37a85dd8fc179562"; }
+/******/ 		__webpack_require__.h = function() { return "4a3294eca21b69e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
